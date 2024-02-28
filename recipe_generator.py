7633bc6d1483e1b6f87208844d82ae919c88a985
shu65 <dolphinripple@gmail.com>
2024-02-28 18:07:56 +0900

    update suggest

--- recipe_generator.py
+++ recipe_generator.py
@@ -43,11 +43,11 @@ def main() -> NoReturn:
         trial = study.ask()
 
         # Ask new parameters
-        temperature = trial.suggest_int("お湯の温度(℃)", 80, 95)
-        bean_per_water = trial.suggest_float("豆の量 (g)/お湯の量(g)", 0.053, 0.1) # 8/150-15/150
-        click_count = trial.suggest_int("ミルのクリック数", 16, 24)
-        steaming_time = trial.suggest_int("蒸らし時間(sec)", 0, 40, step=5)
-        steaming_water_weight = trial.suggest_int("蒸らしのお湯の量(g)", 10, 30, step=10)
+        temperature = trial.suggest_float("お湯の温度(℃)", 80, 95, step=1.0)
+        bean_per_water = trial.suggest_float("豆の量 (g)/お湯の量(g)", 0.053, 0.1, step=0.01) # 8/150-15/150
+        click_count = trial.suggest_float("ミルのクリック数", 16, 24, step=1.0)
+        steaming_time = trial.suggest_float("蒸らし時間(sec)", 0, 40, step=5.0)
+        steaming_water_weight = trial.suggest_float("蒸らしのお湯の量(g)", 10, 30, step=10.0)
 
         # compute paramters
         bean_weight = int(bean_per_water*water_weight)
@@ -56,12 +56,12 @@ def main() -> NoReturn:
         note = textwrap.dedent(
             f"""\
         ## レシピ
-        - お湯の温度(℃): {temperature}
-        - ミルのクリック数: {click_count}
-        - 豆の量(g): {bean_weight}
-        - 蒸らしのお湯の量(g): {steaming_water_weight}
-        - 蒸らし時間(sec): {steaming_time}
-        - 抽出量(g): {water_weight}
+        - お湯の温度(℃): {int(temperature)}
+        - ミルのクリック数: {int(click_count)}
+        - 豆の量(g): {int(bean_weight)}
+        - 蒸らしのお湯の量(g): {int(steaming_water_weight)}
+        - 蒸らし時間(sec): {int(steaming_time)}
+        - 抽出量(g): {int(water_weight)}
         """
         )
         save_note(trial, note)
