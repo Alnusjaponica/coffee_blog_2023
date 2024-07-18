4ec1105df423b05a1ecc8799344e434ad8f79e20
shemmi <shemmi@preferred.jp>
2024-07-18 16:54:31 +0900

    Update recipe for ColdDrip

--- recipe_generator.py
+++ recipe_generator.py
@@ -12,7 +12,7 @@ def main() -> NoReturn:
     STORAGE_URL = "sqlite:///example.db"
     study = create_study(
         n_generate=2,
-        study_name="Coffee recipe",
+        study_name="ColdDrip Recipe",
         storage=STORAGE_URL,
         sampler=PreferentialGPSampler(seed=42),
         load_if_exists=True,
@@ -20,18 +20,15 @@ def main() -> NoReturn:
 
     register_preference_feedback_component(study, "note")
 
-    water_weight = 150
-    speed = 2 # g/sec
-
+    water_weight = 450
     if len(study.get_trials()) == 0:
         # add default trial.
-        # ref: https://www.youtube.com/watch?v=G7vxam3T5mQ&t=68s
         params = {
-            "お湯の温度(℃)": 86,
-            "豆の量 (g)/お湯の量(g)": 12/150,
-            "ミルのクリック数": 18,
-            "蒸らし時間(sec)": 20,
-            "蒸らしのお湯の量(g)": 20,
+            "抽出環境": "refrigerator",
+            "豆の量 (g)": 40.0,
+            "挽き目": 4.0,
+            "初期化水温": 20.0,
+            "初期化水量": 40.0,
         }
         study.enqueue_trial(params)
 
@@ -43,25 +40,21 @@ def main() -> NoReturn:
         trial = study.ask()
 
         # Ask new parameters
-        temperature = trial.suggest_float("お湯の温度(℃)", 80, 95, step=1.0)
-        bean_per_water = trial.suggest_float("豆の量 (g)/お湯の量(g)", 0.053, 0.1, step=0.01) # 8/150-15/150
-        click_count = trial.suggest_float("ミルのクリック数", 16, 24, step=1.0)
-        steaming_time = trial.suggest_float("蒸らし時間(sec)", 0, 40, step=5.0)
-        steaming_water_weight = trial.suggest_float("蒸らしのお湯の量(g)", 10, 30, step=10.0)
-
-        # compute paramters
-        bean_weight = int(bean_per_water*water_weight)
+        temperature = trial.suggest_categorical("抽出環境", ["room", "refrigerator"])
+        bean_amount = trial.suggest_float("豆の量 (g)", 10, 40, step=1.0)
+        grind_level = trial.suggest_float("挽き目", 0, 14, step=1.0)
+        init_water_temp = trial.suggest_float("初期化水温", 20, 80, step=1.0)
+        init_water_weight = trial.suggest_float("初期化水量", 0, 40, step=10.0)
 
         # Add note
         note = textwrap.dedent(
             f"""\
         ## レシピ
-        - お湯の温度(℃): {int(temperature)}
-        - ミルのクリック数: {int(click_count)}
-        - 豆の量(g): {int(bean_weight)}
-        - 蒸らしのお湯の量(g): {int(steaming_water_weight)}
-        - 蒸らし時間(sec): {int(steaming_time)}
-        - 抽出量(g): {int(water_weight)}
+        - 抽出環境: {temperature}
+        - 豆の量: {bean_amount} g
+        - 挽き目: {grind_level}
+        - 初期化水温: {init_water_temp} °C
+        - 初期化水量: {init_water_weight} g x 3回
         """
         )
         save_note(trial, note)
